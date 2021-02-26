6d189e9daeed4eb01b74a7410df6d03471376ce1
David Yu <david.yu@outlook.com>
2021-02-25 22:05:01 -0800

    Completed ex 1,2,3

--- js/ex2.js
+++ js/ex2.js
@@ -1,36 +1,21 @@
 
 function myFunction() {
-    let myArray = []
-    let message = '';
-    
-    // Load array with values 1 to 10
-    for (i = 1; i < 11; i++) {
-        myArray.push(i);
-    }
-
-    let a = myArray.filter(number => number%2);
-    let b = myArray.filter(number =>!(number%2 || number%5))
-    let c = myArray.filter(number =>!(number%3)).map(x => x ** 2)
-    let d = myArray.filter(number =>!(number%5)).reduce((a, b) => a + b) 
-    let d2 = d ** 2
-
-    console.log(myArray);
-    console.log(a);
-    console.log(b);
-    console.log(c);
-    console.log(d2);
-
-    message = 'a. Odd array values:  ' + a + '<br/>' 
-            + 'b. Array values divisible by 2 or 5 is: ' + b + '<br/>' 
-            + 'c. Array of numbers divisible by 3 squared: ' + c + '<br/>' 
-            + 'd. Square the numbers divisible by 5 is: ' + d2 + '<br/>' 
-
-    document.getElementById("lblMessage").innerHTML  =  message;
-}
-
-
-
-
-function clearValues() {
+    clearValues();
+    displayText400();
+  }
+  
+  const displayText400 = () => {
+    var doc = document.getElementsByClassName("400level");
+        for (var i = 0; i < doc.length; i++) {
+            var val = doc[i].innerText;
+            console.log(val)
+            document.getElementById("lblMessage").innerHTML  += val + '<br/>';
+        }   
+  }
+  
+  
+  function clearValues() {
     document.getElementById("lblMessage").innerHTML  = '';
-}
+  }
+  
+  
--- js/ex3.js
+++ js/ex3.js
@@ -1,69 +1,15 @@
 
-const studentList = [
-    {
-        firstName: "Allan",
-        lastName: "Able",
-        scores: [95,85,92,98]
-    },
-    {
-        firstName: "Amy",
-        lastName: "Alexander",
-        scores: [80,88,100]
-    },
-    {
-        firstName: "Betty",
-        lastName: "Barns",
-        scores: [70,80,90,100]
-    },
-    {
-        firstName: "Bob",
-        lastName: "Bones",
-        scores: [75,85,95,85]
-    },
-    {
-        firstName: "Cindy",
-        lastName: "Chase",
-        scores: [95,90,92,98]
-    },
-    {
-        firstName: "Charles",
-        lastName: "Chips",
-        scores: [88,99,90]
-    },
-];
-
 function myFunction() {
-    let message = '';
-    const classResults = []
-    for (var i = 0; i < studentList.length; i++) {
-        classResults[i] = ({firstName: studentList[i].firstName, 
-                    lastName: studentList[i].lastName, 
-                    minScore: getMin(studentList[i].scores), 
-                    maxScore: getMax(studentList[i].scores), 
-                    avgScore: getAvg(i)
-                })
-         message += "[" + i + "] {firstName: " + studentList[i].firstName + ", lastName: " + studentList[i].lastName + ", minScore: " + getMin(studentList[i].scores) + ", maxScore: " + getMax(studentList[i].scores) + ", avgScore: " + getAvg(i) +  "} <br/>"     
-      }
-
-    console.log(classResults)
-    document.getElementById("lblMessage").innerHTML  = message;
-
-}
-
-function getMin(array) {
-    return Math.min(...array)
-}
-
-function getMax(array) {
-    return Math.max(...array)
-}
-
-function getAvg(i) {
-   let myArray = studentList[i].scores
-   return (myArray.reduce((val, el) => val + el, 0) / myArray.length);
-}
-
-function clearValues() {
-    document.getElementById("lblMessage").innerHTML  = '';
-}
-
+    displayText400();
+  }
+  
+  const displayText400 = () => {
+    var doc = document.getElementsByTagName("p");
+        for (var i = 0; i < doc.length; i++) {
+            var val = doc[i].style.backgroundColor = "yellow";
+        }   
+  }
+  
+  
+  
+  
